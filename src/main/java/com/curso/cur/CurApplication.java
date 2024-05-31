a343034ba454485cab9f32ae08d3a017dcd9eb98
murilogillbert <murilo.gillbert@outlook.com>
2024-05-31 12:31:10 -0400

    first commit

---
+++ src/main/java/com/curso/cur/CurApplication.java
@@ -0,0 +1,13 @@
+package com.curso.cur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurApplication.class, args);
+	}
+
+}
